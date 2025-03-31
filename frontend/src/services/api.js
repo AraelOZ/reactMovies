4efa3f765c18655fb3bb680393e99f802d3864d5
AraelOZ <arael.ortiz@gmail.com>
2025-03-31 16:17:12 -0700

    init project
    Added :
    Components
    Services
    Css
    Pages

---
+++ frontend/src/services/api.js
@@ -0,0 +1,14 @@
+const API_KEY = "836e848bb95f0fe4ef1dae505e93ec2a"
+const BASE_URL = "https://api.themoviedb.org/3"
+
+export const getPopularMovies = async () => {
+    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`)
+    const data = await response.json();
+    return data.results;
+};
+
+export const searchMovies = async (query) => {
+    const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`)
+    const data = await response.json();
+    return data.results;
+};
